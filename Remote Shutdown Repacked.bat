478b6857b1205561fa4e40fb3d6fce7fbf6bbd2b
Grathium <33742269+Grathium@users.noreply.github.com>
2018-12-27 15:30:14 +1000

    Add files via upload

---
+++ Remote Shutdown Repacked.bat
@@ -0,0 +1,15 @@
+@echo off
+color a
+title Remote Shutdown Repacked
+set /a publickey=%random%
+ver >nul
+cls
+
+mkdir "C:\Users\%username%\Desktop\RSR\"
+copy "C:\Windows\System32\shutdown.exe" "C:\Users\%username%\Desktop\RSR\%publickey%.exe"
+echo @echo off >>"C:\Users\%username%\Desktop\RSR\Remote Shutdown Repacked.cmd"
+echo %publickey% -i>>"C:\Users\%username%\Desktop\RSR\Remote Shutdown Repacked.cmd"
+echo exit /B 1>>"C:\Users\%username%\Desktop\RSR\Remote Shutdown Repacked.cmd"
+
+echo Installed Remote Shutdown Repacked!
+pause >nul
