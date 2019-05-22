9d4893dd2113f71678f01567b241eab4a3dc75cc
Grathium <33742269+Grathium@users.noreply.github.com>
2019-05-22 10:32:06 +1000

    Update Number Guesser.py

--- Number Guesser.py
+++ Number Guesser.py
@@ -3,21 +3,20 @@ import random
 #initialization
 favNumber = 0
 generations = 0
-dialationFactor = 100
+maxNumber = 1000
+dialationFactor = maxNumber
 
-print("\nPick a number between 1 and 100000,")
+print("\nPick a number between 1 and ,", maxNumber)
 number = int(input())
 while True:
     generations += 1
 
-    if number > 100000:
+    if number > maxNumber:
         continue
 
     if (favNumber == 0):
-        guess = random.randint(-1,100000)
-        print("my guess is", guess)
-    else:
-        print(favNumber)
+        favNumber = random.randint(-1,maxNumber)
+    print(favNumber) #output the number guess
 
     if number < favNumber:
         favNumber -= (1 * (dialationFactor / generations))
